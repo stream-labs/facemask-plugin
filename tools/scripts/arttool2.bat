16b89006de7f6eb8f060cf4686b3e4a00a64c2a8
Ross Kakuschke <granola@granolaboy.net>
2018-03-07 21:53:51 -0800

    editing mask meta data works. coloring and icons work. saving data works.

--- tools/scripts/arttool2.bat
+++ tools/scripts/arttool2.bat
@@ -9,7 +9,8 @@ rem ^
 
 
 import sys, subprocess, os, json, uuid
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QListWidget, QVBoxLayout, QPushButton, QScrollArea, QMainWindow, QCheckBox, QHBoxLayout, QTextEdit, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QListWidget, QVBoxLayout, QPushButton, QComboBox
+from PyQt5.QtWidgets import QScrollArea, QMainWindow, QCheckBox, QHBoxLayout, QTextEdit, QLineEdit
 from PyQt5.QtGui import QIcon, QBrush, QColor, QFont, QPixmap, QMovie
 
 
@@ -45,6 +46,30 @@ def getFileList(folder):
 
 	return fileList
 
+def svnNeedsUpdate():	
+	# have
+	cmd = 'svn info | grep -i "Last Changed Rev"'
+	revHave = 0
+	for line in execute(cmd):
+		revHave = int(line.split()[-1])
+		
+	# head
+	cmd = 'svn info -r HEAD | grep -i "Last Changed Rev"'
+	revHead = 0
+	for line in execute(cmd):
+		revHead = int(line.split()[-1])
+		
+	return revHead > revHave
+
+def svnNeedsCommit():
+	cmd = 'svn status -uq'
+	needCommit = False
+	for line in execute(cmd):
+		if line.split()[0] in ["A", "M", "D"]:
+			needCommit = True
+	return needCommit
+
+	
 def getMetaFolderName(fbxfile):
 	dn = os.path.dirname(fbxfile)
 	dn = os.path.join(dn, ".art").replace("\\","/")
@@ -59,7 +84,6 @@ def createMetaFolder(folder):
 
 def getMetaFileName(fbxfile):
 	metafolder = getMetaFolderName(fbxfile)
-	createMetaFolder(metafolder)
 	metafile = os.path.join(metafolder, os.path.basename(fbxfile).lower().replace(".fbx",".meta")).replace("\\","/")
 	return metafile
 			
@@ -72,6 +96,8 @@ def writeMetaData(metafile, metadata):
 		print("SVN: " + line)
 
 def createGetMetaData(fbxfile):
+	metafolder = getMetaFolderName(fbxfile)
+	createMetaFolder(metafolder)
 	metafile = getMetaFileName(fbxfile)
 	metadata = dict()
 	if os.path.exists(metafile):
@@ -89,6 +115,7 @@ def createGetMetaData(fbxfile):
 		metadata["uuid"] = str(uuid.uuid4())
 		metadata["depth_head"] = False
 		metadata["is_morph"] = False
+		metadata["is_vip"] = False		
 		metadata["texture_max"] = 256
 		metadata["do_not_release"] = False
 		metadata["license"] = "Copyright 2017 - General Working Inc. - All rights reserved."		
@@ -97,13 +124,63 @@ def createGetMetaData(fbxfile):
 		writeMetaData(metafile, metadata)
 		
 	return metadata
+	
+# checks status of meta data 
+#
+CHECKMETA_GOOD = 0
+CHECKMETA_ERROR = 1
+CHECKMETA_WARNING = 2
+CHECKMETA_NORELEASE = 3
+MASK_UNKNOWN = -1
+MASK_NORMAL = 0
+MASK_MORPH = 1
+def checkMetaData(metadata):
+	masktype = MASK_UNKNOWN
 
+	# is morph
+	if metadata["is_morph"]:
+		masktype = MASK_MORPH
+	else:
+		masktype = MASK_NORMAL
+	
+	# not for release
+	if metadata["do_not_release"]:
+		return CHECKMETA_NORELEASE,masktype
+	
+	# check for errors
+	for field,value in metadata.items():
+		if type(value) is str and critical(field) and len(value) == 0:
+			return CHECKMETA_ERROR,masktype
+			
+	# todo: check for icons
+	
+	# good
+	return CHECKMETA_GOOD,masktype
+	
+def checkMetaDataFile(fbxfile):
+	masktype = MASK_UNKNOWN
+	metafile = getMetaFileName(fbxfile)
+	if not os.path.exists(metafile):
+		return CHECKMETA_ERROR,masktype
+		
+	# read existing metadata
+	metadata = dict()
+	f = open(metafile,"r")
+	metadata = json.loads(f.read())
+	f.close()
+
+	# check it
+	return checkMetaData(metadata)
+	
+	
 	
 FIELD_WIDTH = 150
 PANE_WIDTH = 700
+TEXTURE_SIZES = ["32","64","128","256","512","1024","2048"]
 	
-	
-	
+def critical(field):
+	return field in ["name", "author", "license"]
+
 	
 class ArtToolWindow(QMainWindow): 
 
@@ -126,14 +203,10 @@ class ArtToolWindow(QMainWindow):
 		self.fbxlist.itemClicked.connect(lambda: self.onFbxClicked())
 		self.mainLayout.addWidget(self.fbxlist)
 		
-		# color some items
-		self.fbxlist.item(0).setForeground(QBrush(QColor("#FF0000")))
-		self.fbxlist.item(0).setIcon(QIcon('maskicon.png'))
-		self.fbxlist.item(1).setForeground(QBrush(QColor("#32CD32")))
-		self.fbxlist.item(1).setIcon(QIcon('morphicon.png'))
-		self.fbxlist.item(2).setForeground(QBrush(QColor("#FF7F50")))
-		self.fbxlist.item(2).setIcon(QIcon('maskicon.png'))
-		
+		# color items
+		for idx in range(0, len(self.fbxfiles)):
+			self.setFbxColorIcon(idx)
+			
 		# Show the window
 		self.setCentralWidget(mainPane)
 		self.setGeometry(2000, 100, 1024, 768)
@@ -145,11 +218,11 @@ class ArtToolWindow(QMainWindow):
 		self.createRightPane(None)
 		
 		# test
-		self.metadata = createGetMetaData(self.fbxfiles[0])
+		self.metadata = None 
+		self.currentFbx = -1
+
 	
 	def createTextUI(self, name, field, y):
-		critical = field in ["name", "author", "license"]
-	
 		q = QLabel(name)
 		q.setParent(self.rightPane)
 		q.setGeometry(0, y, FIELD_WIDTH, 36)
@@ -162,7 +235,7 @@ class ArtToolWindow(QMainWindow):
 		q.setParent(self.rightPane)
 		q.setGeometry(FIELD_WIDTH, y, PANE_WIDTH - FIELD_WIDTH, 30)
 		q.setFont(QFont( "Arial", 12, QFont.Bold ))
-		if critical and len(text) == 0:
+		if critical(field) and len(text) == 0:
 			q.setStyleSheet("border: 1px solid #FF0000;")
 		else:
 			q.setStyleSheet("border: 0px;")
@@ -197,6 +270,7 @@ class ArtToolWindow(QMainWindow):
 		isset = self.metadata[field]
 		q = QCheckBox()
 		q.setParent(self.rightPane)
+		q.setChecked(isset)
 		q.setGeometry(FIELD_WIDTH, y, PANE_WIDTH - FIELD_WIDTH, 30)
 		q.setFont(QFont( "Arial", 12, QFont.Bold ))
 		q.stateChanged.connect(lambda state: self.onCheckboxChanged(state, field))
@@ -204,8 +278,55 @@ class ArtToolWindow(QMainWindow):
 	
 		self.paneWidgets[field] = q
 
+	def createTextureSizeUI(self, name, field, y):
+		q = QLabel(name)
+		q.setParent(self.rightPane)
+		q.setGeometry(0, y, FIELD_WIDTH, 36)
+		q.setFont(QFont( "Arial", 12, QFont.Bold ))
+		q.show()
 
+		texsize = self.metadata[field]
+		q = QComboBox()
+		idx = 0
+		selidx = 0
+		for s in TEXTURE_SIZES:
+			if int(s) == texsize:
+				selidx = idx
+			q.addItem(s)
+			idx += 1
+		q.setCurrentIndex(selidx)
+		q.setParent(self.rightPane)
+		q.setGeometry(FIELD_WIDTH, y, 100, 30)
+		q.currentIndexChanged.connect(lambda state: self.onTextureSizeChanged(state, field))
+		q.show()
 	
+		self.paneWidgets[field] = q
+
+	def setFbxColorIconInternal(self, mdc, mt, idx):
+		if mdc == CHECKMETA_GOOD:
+			self.fbxlist.item(idx).setForeground(QBrush(QColor("#32CD32")))
+		elif mdc == CHECKMETA_ERROR:
+			self.fbxlist.item(idx).setForeground(QBrush(QColor("#FF0000")))
+		elif mdc == CHECKMETA_WARNING:
+			self.fbxlist.item(idx).setForeground(QBrush(QColor("#FF7F50")))
+		elif mdc == CHECKMETA_NORELEASE:
+			self.fbxlist.item(idx).setForeground(QBrush(QColor("#000000")))
+			
+		if mt == MASK_UNKNOWN:
+			self.fbxlist.item(idx).setIcon(QIcon("unknownicon.png"))
+		elif mt == MASK_NORMAL:
+			self.fbxlist.item(idx).setIcon(QIcon("maskicon.png"))
+		elif mt == MASK_MORPH:
+			self.fbxlist.item(idx).setIcon(QIcon("morphicon.png"))
+
+	def setFbxColorIcon(self, idx):
+		mdc,mt = checkMetaDataFile(self.fbxfiles[idx])
+		self.setFbxColorIconInternal(mdc, mt, idx)
+
+	def updateFbxColorIcon(self):
+		mdc,mt = checkMetaData(self.metadata)
+		self.setFbxColorIconInternal(mdc, mt, self.currentFbx)
+			
 	# Creates right pane
 	#
 	def createRightPane(self, fbxfile):
@@ -240,6 +361,7 @@ class ArtToolWindow(QMainWindow):
 		q.setFont(QFont( "Arial", 14, QFont.Bold ))
 		q.show()
 		
+		# widgets below stored in this dict
 		self.paneWidgets = dict()
 		
 		# name
@@ -271,9 +393,17 @@ class ArtToolWindow(QMainWindow):
 		y += dy
 		self.createCheckboxUI("Is a Morph", "is_morph", y)
 	
+		# is_vip
+		y += dy
+		self.createCheckboxUI("V.I.P. Mask", "is_vip", y)
+	
 		# do_not_release
 		y += dy
 		self.createCheckboxUI("Do Not Release", "do_not_release", y)
+	
+		# texture_max
+		y += dy
+		self.createTextureSizeUI("Max Texture Size", "texture_max", y)
 	
 		# license
 		y += dy
@@ -287,17 +417,26 @@ class ArtToolWindow(QMainWindow):
 	# FBX file clicked in list
 	#
 	def onFbxClicked(self):
-		fbxfile = self.fbxfiles[self.fbxlist.currentRow()]
+		if self.metadata:
+			fbxfile = self.fbxfiles[self.currentFbx]
+			metafile = getMetaFileName(fbxfile)
+			writeMetaData(metafile, self.metadata)
+			
+		self.currentFbx = self.fbxlist.currentRow()
+		fbxfile = self.fbxfiles[self.currentFbx]
+		self.metadata = createGetMetaData(fbxfile)
+		self.updateFbxColorIcon()
 		self.createRightPane(fbxfile)
 
+		
 	# text field changed
 	def onTextFieldChanged(self, text, field):
-		critical = field in ["name", "author", "license"]
 		self.metadata[field] = text
-		if critical and len(text) == 0:
+		if critical(field) and len(text) == 0:
 			self.paneWidgets[field].setStyleSheet("border: 1px solid #FF0000;")
 		else:
-			self.paneWidgets[field].setStyleSheet("border: 0px;")		
+			self.paneWidgets[field].setStyleSheet("border: 0px;")
+		self.updateFbxColorIcon()
 			
 	# checkbox changed
 	def onCheckboxChanged(self, state, field):
@@ -305,9 +444,24 @@ class ArtToolWindow(QMainWindow):
 			self.metadata[field] = False
 		else:
 			self.metadata[field] = True
+		self.updateFbxColorIcon()
 		
+	# texsize changed
+	def onTextureSizeChanged(self, state, field):
+		self.metadata[field] = int(TEXTURE_SIZES[state])
+		
+
+
+
+
+
+
+
 		
 if __name__ == '__main__':
+	
+	if svnNeedsCommit() :
+		print("need commit")
 	
 	# We're a Qt App
 	app = QApplication(sys.argv)
