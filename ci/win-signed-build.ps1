adc6ce2de805440350ffdf5e25c683629f61eb2f
Branka Lakic <brankalakic@gmail.com>
2019-01-16 15:39:54 -0800

    Fix path to the key

--- ci/win-signed-build.ps1
+++ ci/win-signed-build.ps1
@@ -1,4 +1,4 @@
-& secure-file\tools\secure-file -decrypt CI\streamlabsp12.pfx.enc -secret "$env:StreamlabsPfxSecret" -out CI\streamlabsp12.pfx
+& secure-file\tools\secure-file -decrypt c:\projects\source\ci\streamlabsp12.pfx.enc -secret "$env:StreamlabsPfxSecret" -out CI\streamlabsp12.pfx
 
 if ($LASTEXITCODE -ne 0) {
 	exit 1
@@ -6,7 +6,7 @@ if ($LASTEXITCODE -ne 0) {
 
 Get-ChildItem -Recurse  "distribute" -Include "*.dll","*.node","*.exe" |
 Foreach-Object {
-	& "$env:SignTool" sign /as /p "$env:StreamlabsSecret" /f CI\streamlabsp12.pfx $_.FullName
+	& "$env:SignTool" sign /as /p "$env:StreamlabsSecret" /f c:\projects\source\ci\streamlabsp12.pfx $_.FullName
 	if ($LASTEXITCODE -ne 0) {
 		exit 1
 	}
