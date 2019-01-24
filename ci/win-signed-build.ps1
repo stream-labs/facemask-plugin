a45e4c99f01dc608206e8be6a9eb97b00791f42d
Branka Lakic <brankalakic@gmail.com>
2019-01-23 16:01:25 -0800

    Fix build directory name

--- ci/win-signed-build.ps1
+++ ci/win-signed-build.ps1
@@ -4,7 +4,7 @@ if ($LASTEXITCODE -ne 0) {
 	exit 1
 }
 
-Get-ChildItem -Recurse  "distribute/slobs/RelWithDebugInfo/" -Include "*.dll","*.node","*.exe" |
+Get-ChildItem -Recurse  "distribute/slobs/RelWithDebInfo/" -Include "*.dll","*.node","*.exe" |
 Foreach-Object {
 	& "$env:SignTool" sign /as /p "$env:StreamlabsSecret" /f c:\projects\source\ci\streamlabsp12.pfx $_.FullName
 	if ($LASTEXITCODE -ne 0) {
