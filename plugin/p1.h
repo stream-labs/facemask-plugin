d4e666722203bc25de3bb52841d55ec6d876b13a
glaba13 <glaba13@freeuni.edu.ge>
2019-01-30 04:04:30 -0500

    Add function types

--- plugin/p1.h
+++ plugin/p1.h
@@ -1,14 +1,12 @@
-#include "Config.hpp"
 
-extern __declspec(dllexport) int test();
-extern __declspec(dllexport) bool obs_module_load1(void);
-extern __declspec(dllexport) void obs_module_unload1(void);
-extern __declspec(dllexport) const char* obs_module_name1();
-extern __declspec(dllexport) const char* obs_module_description1();
-extern __declspec(dllexport) void obs_module_set_pointer1(obs_module_t *module);
-extern __declspec(dllexport) uint32_t obs_module_ver1(void);
-extern __declspec(dllexport) obs_module_t *obs_current_module1(void);
-extern __declspec(dllexport) const char *obs_module_text1(const char *val);
-extern __declspec(dllexport) bool obs_module_get_string1(const char *val, const char **out);
-extern __declspec(dllexport) void obs_module_set_locale1(const char *locale);
-extern __declspec(dllexport) void obs_module_free_locale1(void);
+typedef bool(*obs_module_load_fun)(void);
+typedef void(*obs_module_unload_fun)(void);
+typedef const char* (*obs_module_name_fun)();
+typedef const char* (*obs_module_description_fun)();
+typedef void(*obs_module_set_pointer_fun)(obs_module_t *module);
+typedef uint32_t(*obs_module_ver_fun)(void);
+typedef obs_module_t *(*obs_current_module_fun)(void);
+typedef const char *(*obs_module_text_fun)(const char *val);
+typedef bool(*obs_module_get_string_fun)(const char *val, const char **out);
+typedef void(*obs_module_set_locale_fun)(const char *locale);
+typedef void(*obs_module_free_locale_fun)(void);
