ef8d628cfb4dbd10dacc86d96fd336a32d579b57
glaba13 <glaba13@freeuni.edu.ge>
2019-03-25 13:48:28 -0400

    Replace alndmark model name

--- smll/FaceDetector.cpp
+++ smll/FaceDetector.cpp
@@ -29,7 +29,7 @@
 #define NUM_HULL_POINTS			(28 * 2 * 2 * 2)
 #define NUM_HULL_POINT_DIVS		(3)
 
-static const char* const kFileShapePredictor68 = "shape_predictor_68_face_landmarks.dat";
+static const char* const kFileShapePredictor68 = "SL_SP_v1.2.dat";
 
 
 using namespace dlib;
